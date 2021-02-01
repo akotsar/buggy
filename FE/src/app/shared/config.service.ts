9745b50f438084fa9c927ae3ee91d7af8f87854d
Andrey Kotsar <andrey.kotsar@gmail.com>
2021-02-01 15:10:03 +1300

    Fixed API URLs.

--- FE/src/app/shared/config.service.ts
+++ FE/src/app/shared/config.service.ts
@@ -5,8 +5,8 @@ export class ConfigService {
     serviceUrl: string;
 
     constructor() {
-        this.serviceUrl = process.env.ENV === 'build' 
-            ? 'https://k51qryqov3.execute-api.ap-southeast-2.amazonaws.com/prod' 
+        this.serviceUrl = process.env.ENV === 'build'
+            ? 'https://k51qryqov3.execute-api.ap-southeast-2.amazonaws.com/prod'
             : 'http://localhost:3000';
     }
 }
